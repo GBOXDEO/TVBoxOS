8182ac4ee2ee535333a931dc7d5520b7ac29f51a
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-02-12 03:54:07 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://152.32.231.214/index_9978.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
